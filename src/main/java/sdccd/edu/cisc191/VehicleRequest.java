80d0ea754eba6011507292c440b18b4ca02fa401
DJDEVERA <deveradarnell@gmail.com>
2022-10-16 23:01:09 -0700

    Commit

--- src/main/java/sdccd/edu/cisc191/VehicleRequest.java
+++ src/main/java/sdccd/edu/cisc191/VehicleRequest.java
@@ -4,14 +4,40 @@ import java.io.Serializable;
 
 public class VehicleRequest implements Serializable {
 
+    private int year;
+    private String make;
+    private String model;
     public String requested;
-    public VehicleRequest(int year, String manufacturer, String model){
+
+    public VehicleRequest(int year, String make, String model){
 
         // client request for vehicle year, manufacturer, and model
-        requested = "You have requested: " + year + " " + manufacturer + " " + model + ".";
+        requested = "You have requested: " + year + " " + make + " " + model + ".";
 
     }
 
+    // get and set year
+    public int getYear() {
+        return year;
+    }
+    public void setYear() {
+        this.year = year;
+    }
 
+    // get and set model
+    public String getModel() {
+        return model;
+    }
+    public void setModel() {
+        this.model = model;
+    }
+
+    // get and set make
+    public String getMake() {
+        return make;
+    }
+    public void setMake() {
+        this.make = make;
+    }
 
 }
--- src/main/java/sdccd/edu/cisc191/VehicleResponse.java
+++ src/main/java/sdccd/edu/cisc191/VehicleResponse.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 
 public class VehicleResponse implements Serializable {
 
+    private VehicleRequest request;
+    private int miles;
+    private int price;
+    private int seats;
+    private int doors;
+    private String[] options;
     public String vehicleInfo;
+
     public VehicleResponse(VehicleRequest request, int miles, int price, int seats, int doors, String[] options){
 
         // writes vehicleInfo to client
@@ -15,7 +22,53 @@ public class VehicleResponse implements Serializable {
                 + "\n This vehicle also features " + Arrays.toString(options) + "."
                 + "\n The price for this vehicle will cost: $" + price + ".";
 
+    }
 
+    // get and set requests
+    public VehicleRequest getRequest(){
+        return request;
+    }
+    public void setRequest(){
+        this.request = request;
+    }
+
+    // get and set miles
+    public int getMiles() {
+        return miles;
+    }
+    public void setMiles(){
+        this.miles = miles;
+    }
+
+    // get and set price
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(){
+        this.price = price;
+    }
+
+    // get and set seats
+    public int getSeats(){
+        return seats;
+    }
+    public void setSeats(){
+        this.seats = seats;
+    }
+
+    // get and set doors
+    public int getDoors(){
+        return doors;
+    }
+    public void setDoors(){
+        this.doors = doors;
+    }
+    // get and set options
+    public String[] getOptions(){
+        return options;
+    }
+    public void setOptions(){
+        this.options = options;
     }
 
 }
